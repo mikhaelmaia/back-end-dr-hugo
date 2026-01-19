13e32f5102582b267caae04b2cfd6209cb695dcb
Robert-Martins <robertcantares@gmail.com>
2026-01-19 18:16:02 -0300

    feat(base-project): configurar aplicação

--- dr-hugo-back-end/src/app.module.ts
+++ dr-hugo-back-end/src/app.module.ts
@@ -1,10 +1,42 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ManagerModule } from './modules/manager.module';
+import { IsUniqueConstraint } from './core/vo/validators/is-unique.validator';
+import { ExistsInValidator } from './core/vo/validators/exists-in.validator';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { TimeoutInterceptor } from './core/config/interceptors/timeout.interceptor';
+import { AuthGuard } from './core/config/security/auth.guard';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './core/config/environment/configuration';
+import { DatabaseModule } from './core/config/database/database.module';
+import { CoreModule } from './core/modules/core.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    ScheduleModule.forRoot(),
+    CoreModule,
+    ManagerModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    IsUniqueConstraint,
+    ExistsInValidator,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TimeoutInterceptor,
+    },
+  ],
 })
 export class AppModule {}
--- dr-hugo-back-end/src/main.ts
+++ dr-hugo-back-end/src/main.ts
@@ -1,8 +1,75 @@
-import { NestFactory } from '@nestjs/core';
+import {
+  BadRequestException,
+  ClassSerializerInterceptor,
+  ExceptionFilter,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { useContainer, ValidationError } from 'class-validator';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './core/config/exceptions/all-exceptions.filter';
+import { corsOptions } from './core/config/security/security.providers';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
+const extractErrorMessages = (errors: ValidationError[]): string[] => {
+  const messages: string[] = [];
+
+  errors.forEach((error) => {
+    if (error.constraints) {
+      messages.push(...Object.values(error.constraints));
+    }
+    if (error.children && error.children.length > 0) {
+      messages.push(...extractErrorMessages(error.children));
+    }
+  });
+
+  return messages;
+};
+
+const configAppDocument = (app: INestApplication): void => {
+  const options = new DocumentBuilder()
+    .setTitle('Dr Hugo API')
+    .setDescription('API for Dr Hugo Platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+};
+
+const configAppPipes = (): ValidationPipe[] => {
+  return [
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: async (errors: ValidationError[]) => {
+        const errorMessages = extractErrorMessages(errors);
+        return new BadRequestException(errorMessages.toString());
+      },
+    }),
+  ];
+};
+
+const configAppFilters = (app: INestApplication): ExceptionFilter[] => {
+  return [new AllExceptionsFilter(app.get(HttpAdapterHost))];
+};
+
+const configAppInterceptors = (app: INestApplication): void => {
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+};
+
+const configApp = (app: INestApplication): void => {
+  app.useGlobalPipes(...configAppPipes());
+  app.useGlobalFilters(...configAppFilters(app));
+  configAppInterceptors(app);
+  configAppDocument(app);
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+};
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule, { cors: corsOptions });
+  configApp(app);
+  await app.listen(process.env.PORT || 3000);
+};
+
 bootstrap();
