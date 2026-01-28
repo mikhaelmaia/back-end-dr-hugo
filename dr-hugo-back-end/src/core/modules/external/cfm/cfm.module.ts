16d1a1590ec6acec5e6b2fa41ce911d87d9ccb34
Robert-Martins <robertcantares@gmail.com>
2026-01-28 14:20:37 -0300

    feat(cfm): ajustar dependências do módulo

--- dr-hugo-back-end/src/core/modules/external/cfm/cfm.module.ts
+++ dr-hugo-back-end/src/core/modules/external/cfm/cfm.module.ts
@@ -1,7 +1,16 @@
 import { Module } from "@nestjs/common";
 import { CfmService } from "./cfm.service";
+import { ConfigModule } from "@nestjs/config";
+import { HttpModule } from "@nestjs/axios";
 
 @Module({
+  imports: [
+    ConfigModule,
+    HttpModule.register({
+      timeout: 30000,
+      maxRedirects: 3,
+    }),
+  ],
     providers: [CfmService],
     exports: [CfmService],
 })
