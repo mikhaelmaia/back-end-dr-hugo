8e4cb31799b6a3779dcda854fcd7f4db9e7f9805
Robert-Martins <robertcantares@gmail.com>
2026-01-29 14:00:40 -0300

    feat(enums): definição de enums

--- dr-hugo-back-end/src/core/vo/consts/enums.ts
+++ dr-hugo-back-end/src/core/vo/consts/enums.ts
@@ -58,6 +58,71 @@ export enum TermsType {
   PRIVACY_POLICY = 'privacy_policy',
   /** Termos de Serviço da plataforma */
   TERMS_OF_SERVICE = 'terms_of_service',
+}
+
+/** Especializações médicas reconhecidas pelo Conselho Federal de Medicina (CFM)
+ * 
+ * @swagger
+ * @enum {string}
+ */
+export enum DoctorSpecializationType {
+  ACUPUNCTURE = 'Acupuntura',
+  ALLERGY_IMMUNOLOGY = 'Alergia e imunologia',
+  ANESTHESIOLOGY = 'Anestesiologia',
+  ANGIOLOGY = 'Angiologia',
+  CARDIOLOGY = 'Cardiologia',
+  CARDIOVASCULAR_SURGERY = 'Cirurgia cardiovascular',
+  HAND_SURGERY = 'Cirurgia da mão',
+  HEAD_AND_NECK_SURGERY = 'Cirurgia de cabeça e pescoço',
+  DIGESTIVE_SYSTEM_SURGERY = 'Cirurgia do aparelho digestivo',
+  GENERAL_SURGERY = 'Cirurgia geral',
+  ONCOLOGIC_SURGERY = 'Cirurgia oncológica',
+  PEDIATRIC_SURGERY = 'Cirurgia pediátrica',
+  PLASTIC_SURGERY = 'Cirurgia plástica',
+  THORACIC_SURGERY = 'Cirurgia torácica',
+  VASCULAR_SURGERY = 'Cirurgia vascular',
+  INTERNAL_MEDICINE = 'Clínica médica',
+  COLOPROCTOLOGY = 'Coloproctologia',
+  DERMATOLOGY = 'Dermatologia',
+  ENDOCRINOLOGY_METABOLOGY = 'Endocrinologia e metabologia',
+  ENDOSCOPY = 'Endoscopia',
+  GASTROENTEROLOGY = 'Gastroenterologia',
+  MEDICAL_GENETICS = 'Genética médica',
+  GERIATRICS = 'Geriatria',
+  GYNECOLOGY_OBSTETRICS = 'Ginecologia e obstetrícia',
+  HEMATOLOGY_HEMOTHERAPY = 'Hematologia e hemoterapia',
+  HOMEOPATHY = 'Homeopatia',
+  INFECTOLOGY = 'Infectologia',
+  MASTOLOGY = 'Mastologia',
+  EMERGENCY_MEDICINE = 'Medicina de emergência',
+  FAMILY_COMMUNITY_MEDICINE = 'Medicina de família e comunidade',
+  OCCUPATIONAL_MEDICINE = 'Medicina do trabalho',
+  TRAFFIC_MEDICINE = 'Medicina do tráfego',
+  SPORTS_MEDICINE = 'Medicina esportiva',
+  PHYSICAL_REHABILITATION_MEDICINE = 'Medicina física e reabilitação',
+  INTENSIVE_CARE_MEDICINE = 'Medicina intensiva',
+  LEGAL_MEDICINE = 'Medicina legal e perícia médica',
+  NUCLEAR_MEDICINE = 'Medicina nuclear',
+  PREVENTIVE_SOCIAL_MEDICINE = 'Medicina preventiva e social',
+  NEPHROLOGY = 'Nefrologia',
+  NEUROSURGERY = 'Neurocirurgia',
+  NEUROLOGY = 'Neurologia',
+  NUTROLOGY = 'Nutrologia',
+  OPHTHALMOLOGY = 'Oftalmologia',
+  CLINICAL_ONCOLOGY = 'Oncologia clínica',
+  ORTHOPEDICS_TRAUMATOLOGY = 'Ortopedia e traumatologia',
+  OTORHINOLARYNGOLOGY = 'Otorrinolaringologia',
+  PATHOLOGY = 'Patologia',
+  CLINICAL_PATHOLOGY = 'Patologia clínica/medicina laboratorial',
+  PEDIATRICS = 'Pediatria',
+  PULMONOLOGY = 'Pneumologia',
+  PSYCHIATRY = 'Psiquiatria',
+  RADIOLOGY_IMAGING = 'Radiologia e diagnóstico por imagem',
+  RADIOTHERAPY = 'Radioterapia',
+  RHEUMATOLOGY = 'Reumatologia',
+  UROLOGY = 'Urologia',
+}
+
 export enum DoctorSituation {
   REGULAR = 'A',
   SUSPENSAO_PARCIAL_PERMANENTE = 'B',
@@ -85,4 +150,62 @@ export enum DoctorRegistrationType {
   TEMPORARIA = 'T',
   ESTUDANTE_ESTRANGEIRO = 'E',
 }
+
+export enum BrazilianState {
+  AC = 'AC',
+  AL = 'AL',
+  AP = 'AP',
+  AM = 'AM',
+  BA = 'BA',
+  CE = 'CE',
+  DF = 'DF',
+  ES = 'ES',
+  GO = 'GO',
+  MA = 'MA',
+  MT = 'MT',
+  MS = 'MS',
+  MG = 'MG',
+  PA = 'PA',
+  PB = 'PB',
+  PR = 'PR',
+  PE = 'PE',
+  PI = 'PI',
+  RJ = 'RJ',
+  RN = 'RN',
+  RS = 'RS',
+  RO = 'RO',
+  RR = 'RR',
+  SC = 'SC',
+  SP = 'SP',
+  SE = 'SE',
+  TO = 'TO',
+}
+
+/**
+ * Tipos de instituições médicas
+ * 
+ * @swagger
+ * @enum {string}
+ */
+export enum MedicalInstitutionType {
+  CONSULTORIO_CLINICA = 'Consultório / Clínica',
+  ATENCAO_PRIMARIA = 'Atenção Primária',
+  HOSPITAL_GERAL = 'Hospital Geral',
+  HOSPITAL_ESPECIALIZADO = 'Hospital Especializado',
+  URGENCIA_EMERGENCIA = 'Urgência / Emergência',
+  DIAGNOSTICO_POR_IMAGEM = 'Diagnóstico por Imagem',
+  LABORATORIO = 'Laboratório',
+  SADT_DIAGNOSE_TERAPIA = 'SADT / Diagnose e Terapia',
+  BANCOS_HEMOTERAPIA = 'Bancos / Hemoterapia',
+  DOMICILIAR = 'Domiciliar',
+  REGULACAO_GESTAO_ADMINISTRATIVO = 'Regulação / Gestão / Administrativo',
+  PERICIA = 'Perícia',
+  OUTROS = 'Outros',
+}
+
+export enum CompanyType {
+
+  HEADQUARTERS = 'Matriz',
+  BRANCH = 'Filial',
+
 }
